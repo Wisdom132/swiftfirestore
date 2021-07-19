f50f63492ed91f2988b1e36f41ebef2a43f900ff
wisdom Ekpot <wisdomekpot@gmail.com>
2021-07-19 09:11:10 +0100

    Initial Commit

---
+++ funfirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  funfirebase
+//
+//  Created by Wisdom Ekpot on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ funfirebase/funfirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  funfirebaseApp.swift
+//  funfirebase
+//
+//  Created by Wisdom Ekpot on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct funfirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
